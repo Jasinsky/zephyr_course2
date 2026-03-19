3c244bf7ec54af88e99acf6ec9ef632f8cc27bd2
Daniel Jasinski <jasinskidaniel95szcz@gmail.com>
2026-03-19 08:51:35 +0100

    chore: simple shell command
    Signed-off-by: Daniel Jasinski <jasinskidaniel95szcz@gmail.com>

--- COURSE_README.md
+++ COURSE_README.md
@@ -5,3 +5,9 @@ Passing extra configuration files
 ```
 west build -b native_sim  -- -DCONF_FILE=debug.conf
 ```
+
+Running target
+
+```
+west build -b native_sim  -- -DCONF_FILE=debug.conf -t run
+```
--- app/src/main.c
+++ app/src/main.c
@@ -1,11 +1,26 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/shell/shell.h>
 
 LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
+static int cmd_greet(const struct shell *sh, size_t argc, char **argv)
+{
+	const char *name = (argc > 1) ? argv[1] : "World";
+
+	shell_print(sh, "Hello, %s! Greetings from Zephyr!", name);
+	return 0;
+}
+
+SHELL_CMD_REGISTER(greet, NULL, "Print a greeting. Usage: greet [name]", cmd_greet);
+
 int main(void)
 {
-	LOG_INF("Hello World from Zephyr!");
-	return 0;
+       LOG_INF("Hello World from Zephyr!");
+       while (true) {
+	       LOG_INF("Sleeping for 1 second...");
+	       k_sleep(K_SECONDS(1));
+       }
+       return 0;
 }
 
